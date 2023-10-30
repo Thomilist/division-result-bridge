09ecb0e3959814010871a2e2745f0443df8d6c06
Thomas Emil Jensen <Thomas_98@hotmail.dk>
2023-10-30 21:44:27 +0100

    Filled out the privacy page
    - No cookies :)

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -8,4 +8,34 @@
     </title>
 </svelte:head>
 
-<h1>{$t("page.privacy.title")}</h1>
+<h1>
+    {$t("page.privacy.title")}
+</h1>
+
+<p>
+    {$t("page.privacy.intro")}
+</p>
+
+<p>
+    {@html $t("page.privacy.tldr")}
+</p>
+
+<h2>
+    {$t("page.privacy.website.title")}
+</h2>
+
+<p>
+    {@html $t("page.privacy.website.content")}
+</p>
+
+<h2>
+    {$t("page.privacy.application.title")}
+</h2>
+
+<p>
+    {@html $t("page.privacy.application.settings")}
+</p>
+
+<p>
+    {@html $t("page.privacy.application.results")}
+</p>
