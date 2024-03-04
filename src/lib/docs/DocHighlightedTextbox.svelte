7ccf10b15ec1f6455503c7f9dd8bededb40ab118
Thomas Emil Jensen <Thomas_98@hotmail.dk>
2024-03-05 00:09:08 +0100

    Highlighted text box component

---
+++ src/lib/docs/DocHighlightedTextbox.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { t } from "svelte-intl-precompile";
+
+    export let style: "tip" | "warning";
+</script>
+
+<style lang="scss">
+    @import "$styles/doc-highlighted-textbox.scss";
+</style>
+
+<div class="doc-highlighted-textbox">
+    <b>
+        {#if style === "tip"}
+            ℹ️
+        {:else if style === "warning"}
+            ⚠️
+        {/if}
+
+        {$t(`ui.${style}`)}
+    </b>
+    
+    <slot/>
+</div>
